966041c5f8e32051dbd6316df7e5e955cae31221
Gabriel Dias <contatogbd@gmail.com>
2026-05-28 21:43:18 -0300

    fix: CORS

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=settings.cors_origins_list,
     allow_credentials=settings.cors_allow_credentials,
     allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
 
 
